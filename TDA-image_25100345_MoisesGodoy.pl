abbdcb9a36b0fcbe292bc357720061b8687dd936
MoisesGodoy17 <moises.godoy@usach.cl>
2022-10-20 14:03:31 -0300

    funcion RGBToHex lista

--- TDA-image_25100345_MoisesGodoy.pl
+++ TDA-image_25100345_MoisesGodoy.pl
@@ -59,6 +59,52 @@ imageCrop(I, X1, Y1, X2, Y2, I2):-
     NewY is ((Y1 - Y2) - 1)*(-1),
     image(NewX, NewY, L, I2).
 
+makeHex(Num, NewCan):-
+    Rest is div(Num, 16),
+	M is (Num - (16 * Rest)),
+    hexConvert(Rest, _, X), 
+    hexConvert(M, _, Y), 
+    atomic_concat(X, Y, NewCan).
+
+hexConvert(0, 0, 0).
+hexConvert(1, 1, 1).
+hexConvert(2, 2, 2).
+hexConvert(3, 3, 3).
+hexConvert(4, 4, 4).
+hexConvert(5, 5, 5).
+hexConvert(6, 6, 6).
+hexConvert(7, 7, 7).
+hexConvert(8, 8, 8).
+hexConvert(9, 9, 9).
+hexConvert(10, "A", "A").
+hexConvert(11, "B", "B").
+hexConvert(12, "C", "C").
+hexConvert(13, "D", "D").
+hexConvert(14, "E", "E").
+hexConvert(15, "F", "F").
+
+rgbToHex([], _, _, ListaPixeles, ListaPixeles).
+rgbToHex([Pixel|Cdr], Ancho, Largo, ListaAux, L):-
+    pixrgb-d(X, Y, R, G, B, Depth, Pixel),
+    makeHex(R, Cr),
+    makeHex(G, Cg),
+    makeHex(B, Cb),
+    atomic_concat("#", Cr, Hex),
+    atomic_concat(Hex, Cg, TempHex),
+    atomic_concat(TempHex, Cb, AuxHex),
+    pixbit-d(Y, X, AuxHex, Depth, PixelHex),
+    agregar(PixelHex, ListaAux, ListaPixeles),
+    rgbToHex(Cdr, Ancho, Largo, ListaPixeles, L).
+
+imageRGBToHex(I, I2):-
+    image(X, Y, Pixeles, I),
+    rgbToHex(Pixeles, X, Y, _, L),
+    image(X, Y, L, I2).
+
+imageToHistogram( I, Histograma):-
+    image(X, Y, Pixeles, I),
+    histograma
+
 % pixmap de 2X2.
 % pixbit-d( 0, 0, 1, 10, PA), pixbit-d( 0, 1, 0, 20, PB), 
 % pixbit-d( 1, 0, 0, 30, PC), pixbit-d( 1, 1, 1, 4, PD), 
@@ -70,36 +116,11 @@ imageCrop(I, X1, Y1, X2, Y2, I2):-
 % pixbit-d( 2, 0, 1, 50, PG), pixbit-d(2, 1, 0, 60, PH), pixbit-d(2, 2, 1, 55, PI), 
 % image( 3, 3, [PA, PB, PC, PD, PE, PF, PG, PH, PI], I), imageCrop( I , 1, 2, 2, 2, I2 ).
 
-makeHex(Num, ListRest, L):-
-    Rest is div(Num, 16),
-	M is (Num - (16 * Rest)),
-    agregar(Rest, L, ListRest),
-    agregar(M, L, ListRest).
-
-
-rgbToHex([], _, _, ListaPixeles, ListaPixeles).
-rgbToHex([Pixel|Cdr], Ancho, Largo, _, ListaAux):-
-    pixrgb-d(_, _, R, G, B, _, Pixel),
-    makeHex(R, ListaPixeles, L),
-    makeHex(G, ListaPixeles, L),
-    makeHex(B, ListaPixeles, L),
-    %agregar(Pixel, ListaAux, ListaPixeles),
-    rgbToHex(Cdr, Ancho, Largo, ListaPixeles, ListaAux).
-
-imageRGBToHex(I, I2):-
-    image(X, Y, Pixeles, I),
-    rgbToHex(Pixeles, X, Y, _, L),
-    image(X, Y, L, I2).
-
-
 %pixRGB-d -> rgbTohex
 % pixrgb-d( 0, 0, 255, 255, 1, 10, PA), pixrgb-d( 0, 1, 0, 0, 255, 20, PB), 
 % pixrgb-d( 1, 0, 0, 0, 0, 30, PC), pixrgb-d( 1, 1, 255, 255, 255, 4, PD), 
 % image( 2, 2, [PA, PB, PC, PD], I), imageRGBToHex( I, I2 ).
 
-%N is div(255, 16),
-	% M is (255 - (16 * N)),
-	% (   N = 12
-	% ->  Acc = "C"
-	% ;   Acc = N
-	% ).
+% pixrgb-d( 0, 0, 200, 255, 101, 10, PA), pixrgb-d( 0, 1, 199, 0, 10, 20, PB), 
+% pixrgb-d( 1, 0, 1, 155, 239, 30, PC), pixrgb-d( 1, 1, 123, 255, 89, 4, PD), 
+% image( 2, 2, [PA, PB, PC, PD], I), imageRGBToHex( I, I2 ).
