fa106433b2b7809f7c2c6276a8cc22ad46848139
MoisesGodoy17 <moises.godoy@usach.cl>
2022-11-01 22:41:19 -0300

    tda pixbit

---
+++ tda_pixbit_25100345_moises_godoy.pl
@@ -0,0 +1,8 @@
+% Meta principal: pixbit
+% Meta secundaria: Null
+% Dom: Int X Int X Int X Int X pixbit
+% Rec: pixbit
+% Descripcion: regla Constructora del TDA pixbit
+% Recursion: NULL
+
+pixbit-d(X, Y, Bit, Depth, [X, Y, Bit, Depth]).
