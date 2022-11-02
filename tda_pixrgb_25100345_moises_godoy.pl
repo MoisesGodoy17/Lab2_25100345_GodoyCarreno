1f53024af4e7e152bc9ce30cfaa3e86518ef3fab
MoisesGodoy17 <moises.godoy@usach.cl>
2022-11-01 22:41:56 -0300

    tda pixrgb

---
+++ tda_pixrgb_25100345_moises_godoy.pl
@@ -0,0 +1,8 @@
+% Meta principal: pixrgb
+% Meta secundaria: Null
+% Dom: Int X Int X Int X Int X Int X Int X pixrgb
+% Rec: pixrgb
+% Descripcion: regla Constructora del TDA pixrgb
+% Recursion: NULL
+
+pixrgb-d(X, Y, R, G, B, Depth, [X, Y, R, G, B, Depth]).
