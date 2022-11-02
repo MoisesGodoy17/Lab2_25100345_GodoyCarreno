9c183c0494ac47b02342438d119d6d28bff2b24a
MoisesGodoy17 <moises.godoy@usach.cl>
2022-11-01 22:41:38 -0300

    tda pixhex

---
+++ tda_pixhex_25100345_moises_godoy.pl
@@ -0,0 +1,8 @@
+% Meta principal: pixhex
+% Meta secundaria: Null
+% Dom: Int X Int X String X Int X pixhex
+% Rec: pixhex
+% Descripcion: regla Constructora del TDA pixhex
+% Recursion: NULL
+
+pixhex-d(X, Y, Hex, Depth, [X, Y, Hex, Depth]).
